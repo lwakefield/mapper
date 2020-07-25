72dfa4e19485180325587af21b3fb898b5493c98
Lawrence Wakefield <lawrence@iamlawrence.me>
2020-07-25 15:41:41 -0400

    Another attempt at fixing the image url handling

--- app/src/config.js
+++
@@ -1,8 +0,0 @@
-import { stores } from '@sapper/app';
-import { get } from 'svelte/store';
-
-export function getImageProxyHost() {
-    const { session } = stores();
-    const { IMAGE_PROXY_HOST } = get(session);
-    return IMAGE_PROXY_HOST;
-}
