deb8abdaa6765090e5e3f30a9503c6c51d678a81
Lawrence Wakefield <lawrence@iamlawrence.me>
2020-07-26 08:21:59 -0400

    Update home page

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,10 +1,32 @@
 <style>
+    section {
+        max-width: 480px;
+        margin: 0 auto;
+    }
 </style>
 
 <script>
     import * as uuid from 'uuid';
 </script>
 
-<div>
-	<a href="/gm/{uuid.v4()}">New Game</a>
-</div>
+<section>
+
+    <h1>DnDandy</h1>
+
+    <p>DnDandy is a virtual tabletop for D&D with the following features:</p>
+
+    <ul>
+        <li>Map Creation</li>
+        <li>Realtime Sync</li>
+        <li>GM/Player View</li>
+        <li>Fog of War</li>
+        <li>Unlimited maps per game</li>
+        <li>Custom tokens</li>
+        <li>Chat</li>
+        <li>Dice rolling</li>
+    </ul>
+
+    <p>DnDandy was built out of a frustration with Roll20. It is not intended to be a feature-for-feature clone, instead it focuses on the core features (map management, token management and chat), aiming for simplicity, flexibility and speed.</p>
+
+    <p><a href="/gm/{uuid.v4()}">Start a game!</a></p>
+</section>
