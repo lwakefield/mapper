864fdb18309166b0340e458fdd5bbf858445486f
Lawrence Wakefield <lawrence@iamlawrence.me>
2020-07-26 10:02:54 -0400

    Improve tile rendering

--- app/src/components/Cell.svelte
+++ app/src/components/Cell.svelte
@@ -5,8 +5,7 @@
 <rect
     x={x} y={y}
     width={1} height={1}
-    shape-rendering="geometricprecision"
+    shape-rendering="crispEdges"
     vector-effect="non-scaling-stroke"
-    stroke-width="1px"
     {...$$props}
 />
